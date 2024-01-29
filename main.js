72878e0176ec88addead6e932fd016f33c1d4ed5
Mohd Zaid Khan <mdzaidkhan628@gmail.com>
2024-01-29 17:06:12 -0500

    Setup Road and Camera movement

--- main.js
+++ main.js
@@ -4,14 +4,24 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 // Car(x,y,w,h)
-const car = new Car(100, 100,30, 50);
+const road = new Road(canvas.width/2, canvas.width*0.9);
+
+const car = new Car(road.getLaneCenter(1), 700,30, 50);
 
 animate();
-
+console.log(road.borders)
 
 function animate(){
     car.update();
+
     canvas.height=window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.5);
+
+    road.draw(ctx);
     car.draw(ctx);
+    
+    ctx.restore();
     requestAnimationFrame(animate);
 }
